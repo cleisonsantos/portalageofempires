af1193f5dc60db587d546ad589ef3ee368b081cf
cleisonsantos <css.cleison@outlook.com>
2021-08-06 02:36:44 -0300

    Ajuste header civilizações.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch(`http://localhost:8001/api/list-civs`)
+                const res = await fetch(`http://localhost:8000/api/list-civs`)
                 setCivis(await res.json())
             } catch (error) {
                 console.log(error)
@@ -19,7 +19,7 @@ const Header = () => {
         }
         fetchData()
     }, [])
-    
+
     console.log(civis)
     return (
         <header className={styles.navbar}>
@@ -35,22 +35,22 @@ const Header = () => {
 
                         <div>
                             <ul>
-                            {
-                                civis.map((civ, i) => {
-                                    return (
-                                        <li key={i}>
-                                            <Link href='/civis' >
-                                                {/* <div> */}
-                                                <>
-                                                <Image height={10} width={10} src={civ.url_icone || 'favicon.ico'} />
-                                                {civ.name}
-                                                </>
-                                                {/* </div> */}
-                                            </Link>
-                                        </li>
-                                    )
-                                })
-                            }
+                                {
+                                    civis.map((civ, i) => {
+                                        return (
+                                            <li key={i}>
+                                                <Link href='/civis' passHref >
+                                                    <Link href={`/civis/${civ.name}`} passHref >
+                                                        <a href="">
+                                                            <Image height={10} width={10} src={civ.url_icone} alt='' />
+                                                            {civ.name_pt_br}
+                                                        </a>
+                                                    </Link>
+                                                </Link>
+                                            </li>
+                                        )
+                                    })
+                                }
                             </ul>
                         </div>
 
