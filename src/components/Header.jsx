d1dfddbe79e33885cc6d412b762c58c971d8c4f2
cleisonsantos <css.cleison@outlook.com>
2021-08-05 22:19:37 -0300

    Criado o component header carregando as civis da api.

---
+++ src/components/Header.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Header.module.css'
+import logo from '../../public/logo.svg'
+
+const Header = () => {
+
+    const [civis, setCivis] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`http://localhost:8001/api/list-civs`)
+                setCivis(await res.json())
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+    
+    console.log(civis)
+    return (
+        <header className={styles.navbar}>
+            <Link href='/' passHref >
+                <Image height={100} width={250} src={logo} alt='' />
+            </Link>
+            <nav >
+                <ul>
+                    <li>
+                        <Link href='/civis' >
+                            Civilizações
+                        </Link>
+
+                        <div>
+                            <ul>
+                            {
+                                civis.map((civ, i) => {
+                                    return (
+                                        <li key={i}>
+                                            <Link href='/civis' >
+                                                <div>
+                                                <Image height={10} width={10} src={civ.foto_escudo || 'favicon.ico'} />
+                                                {civ.name}
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                            </ul>
+                        </div>
+
+                    </li>
+                    <li>
+                        <Link href='#' >
+                            Unidades
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='#' >
+                            Notícias
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='#' >
+                            Sobre
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <ul>
+                <li>
+                    Buscar
+                </li>
+                <li>
+                    Login
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
